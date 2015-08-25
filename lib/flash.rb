73143d03c95295464051d4c5da519c8efbd823d9
Edmund Wright <edmund.wright@gmail.com>
2015-08-25 17:59:31 -0400

    Continue working on Flash.

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,2 +1,24 @@
 class Flash
+  def initialize(req)
+    cookie = retrieve_cookie(req)
+
+    if cookie
+      from_cookie = JSON.parse(cookie.value)
+      @not_now = from_cookie[:not_now]
+      @now = from_cookie[:now]
+    else
+      @not_now, @now = {}, {}
+    end
+  end
+
+  def now
+  end
+
+  private
+
+  def retrieve_cookie(req)
+    req.cookies.find do |cookie|
+      cookie.name == "_rails_lite_app_flash"
+    end
+  end
 end
