b4cb378b6fc954cd611384ee6532dde0309cbce5
Edmund Wright <edmund.wright@gmail.com>
2015-08-25 12:14:00 -0400

    Continue with Phase2::ControllerBase.

--- lib/phase2/controller_base.rb
+++ lib/phase2/controller_base.rb
@@ -1,25 +1,32 @@
-class Phase2::ControllerBase
-  attr_reader :request, :response
+module Phase2
+  class ControllerBase
+    attr_reader :req, :res
 
-  def initialize(request, response)
-    @request, @response = request, response
-  end
+    def initialize(req, res)
+      @req, @res = req, res
+    end
 
-  def render_content(content, content_type)
-    response.content = content
-    response.content_type = content_type
-    record_response_built
-  end
+    # Raise an error if the developer tries to double render.
+    def render_content(content, content_type)
+      response.content = content
+      response.content_type = content_type
+      record_response_built
+    end
 
-  def redirect_to(url)
-    response.header.location = url
-    response.status = 302
-    record_response_built
-  end
+    def redirect_to(url)
+      response.header.location = url
+      response.status = 302
+      record_response_built
+    end
 
-  private
+    private
 
-  def record_response_built
-    @already_built_response = true
+    def already_built_response?
+      @already_built_response
+    end
+
+    def record_response_built
+      @already_built_response = true
+    end
   end
 end
