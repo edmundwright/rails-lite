2205a1915a372c841aecc5d653202b91d3109f25
Edmund Wright <edmund.wright@gmail.com>
2015-08-25 12:14:19 -0400

    Add project skeleton.

--- bin/p03_template_server.rb
+++ bin/p03_template_server.rb
@@ -1,19 +1,21 @@
 require 'webrick'
+require_relative '../lib/phase3/controller_base'
 
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
 
+class MyController < Phase3::ControllerBase
+  def go
+    render :show
+  end
+end
+
 server = WEBrick::HTTPServer.new(Port: 3000)
-
-server.mount_proc("/") do |request, response|
-  response.content_type = "text/type"
-  response.body = "So you want to go to #{request.path}, eh? Tough luck!"
+server.mount_proc('/') do |req, res|
+  MyController.new(req, res).go
 end
 
-trap('INT') do
-  server.shutdown
-end
-
+trap('INT') { server.shutdown }
 server.start
