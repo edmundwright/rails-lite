39ea62da6ca65e1d318e99750f856ffe05dbe07e
Edmund Wright <edmund.wright@gmail.com>
2015-08-25 10:25:28 -0400

    Make server return taunting message.

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -2,6 +2,11 @@ require 'webrick'
 
 server = WEBrick::HTTPServer.new(Port: 3000)
 
+server.mount_proc("/") do |request, response|
+  response.content_type = "text/type"
+  response.body = "So you want to go to #{request.path}, eh? Tough luck!"
+end
+
 trap('INT') do
   server.shutdown
 end
